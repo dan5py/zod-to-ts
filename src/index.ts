a498798c1cd4505e3722c31437d80e2e2edb7d91
Alve Svarén <alve.svaren@intunio.se>
2024-03-26 15:28:58 +0100

    feat: add default value to generated jsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import ts from 'typescript'
-import { ZodTypeAny } from 'zod'
+import { AnyZodObject, ZodDefault, ZodString, ZodTypeAny } from 'zod'
 import {
 	GetType,
 	GetTypeFunction,
@@ -148,8 +148,22 @@ const zodToTsNode = (
 					type,
 				)
 
+				let descriptionParts: string[] = [];
+
 				if (nextZodNode.description) {
-					addJsDocComment(propertySignature, nextZodNode.description)
+					descriptionParts.push(nextZodNode.description);
+				}
+
+				if (nextZodNode instanceof ZodDefault) {
+					const data = nextZodNode._def.defaultValue();
+					const stringifiedData = JSON.stringify(data);
+					descriptionParts.push(`@default ${stringifiedData}`);
+				}
+
+
+				if (descriptionParts.length > 0) {
+					const suffix = descriptionParts.length > 1 ? '\n' : '';
+					addJsDocComment(propertySignature, descriptionParts.join('\n * ') + suffix)
 				}
 
 				return propertySignature
